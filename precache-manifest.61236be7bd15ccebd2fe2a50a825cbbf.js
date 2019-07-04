e8c211038075c5655fd0192bcfb3122795b8544e
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-07-04 15:10:50 -0400

    Updates

---
+++ precache-manifest.61236be7bd15ccebd2fe2a50a825cbbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238ccb17021c98de28eaceb2308951d9",
+    "url": "/Desafio-MercadoLibre/index.html"
+  },
+  {
+    "revision": "453cfbaa0274cb8ea790",
+    "url": "/Desafio-MercadoLibre/static/css/main.58377ca7.chunk.css"
+  },
+  {
+    "revision": "0c25a46ece054a6a28d9",
+    "url": "/Desafio-MercadoLibre/static/js/2.71c2ab7b.chunk.js"
+  },
+  {
+    "revision": "453cfbaa0274cb8ea790",
+    "url": "/Desafio-MercadoLibre/static/js/main.5f77bb66.chunk.js"
+  },
+  {
+    "revision": "4dd1d6c114cb26f8551c",
+    "url": "/Desafio-MercadoLibre/static/js/runtime~main.cff440a0.js"
+  }
+]);
